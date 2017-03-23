70c239cf5ca9d71d31dda2e5cccf65b164d0dc61
Daniel Wesloh <dfw5129@psu.edu>
2017-03-23 09:59:14 -0400

    Make ensemble integrators work with multiprocessing.
    Fork failures bar this on cygwin.

--- src/inversion/ensemble/integrators.py
+++ src/inversion/ensemble/integrators.py
@@ -7,10 +7,10 @@ from __future__ import absolute_import
 
 import multiprocessing
 import threading
+import abc
 
 import numpy as np
-
-import inversion.integrators
+import six
 
 MAX_PARALLEL = None
 """Passed to :class:`multiprocessing.Pool` constructor
@@ -18,120 +18,184 @@ MAX_PARALLEL = None
 None means use CPU count.
 """
 
+class EnsembleIntegrator(six.with_metaclass(abc.ABCMeta)):
+    """Metaclass for ensemble integrators."""
 
-def serial(func, y0s, tseq, dt, Dfunc=None,
-           integrator=inversion.integrators.scipy_odeint):
-    """Integrate each y0 in `y0s` to times in `tseq`.
-
-    Parameters
-    ----------
-    func: callable(y, t)
-    y0s: np.ndarray[K, N]
-    tseq: sequence[M]
-        tseq[0] := t0
-    Dfunc: callable(y, t)
-        Dfunc[i, j] = d func[i]/d y[j]
-    integrator: callable(func, y0s, tseq, dt, Dfunc
-        The integrator to use
-
-    Returns
-    -------
-    sol_seqs: np.ndarray[M, K, N]
-        Array of solutions to y' = func(y, t)
-    """
-    tseq = np.atleast_1d(tseq)
-    y0s = np.atleast_2d(y0s)
-
-    res = np.empty((len(tseq),) + y0s.shape, y0s.dtype)
-
-    for i in range(res.shape[1]):
-        res[:, i, :] = integrator(func, y0s[i, :], tseq, dt, Dfunc)
-
-    return res
-
-
-def processes(func, y0s, tseq, dt, Dfunc=None,
-              integrator=inversion.integrators.scipy_odeint):
-    """Integrate each y0 in `y0s` to times in `tseq`.
-
-    Break up the work across :const:`MAX_PARALLEL` processes.  Not
-    sure if :func:`scipy.integrators.odeint` is threadsafe enough for
-    this application.  I know the object oriented solvers are not.
-
-    Parameters
-    ----------
-    func: callable(y, t)
-    y0s: np.ndarray[K, N]
-    tseq: sequence[M]
-        tseq[0] := t0
-    Dfunc: callable(y, t)
-        Dfunc[i, j] = d func[i]/d y[j]
-    integrator: callable(func, y0s, tseq, dt, Dfunc
-        The integrator to use
-
-    Returns
-    -------
-    sol_seqs: np.ndarray[M, K, N]
-        Array of solutions to y' = func(y, t)
-
-    """
-    y0s = np.atleast_2d(y0s)
-
-    pool = multiprocessing.Pool(MAX_PARALLEL)
-    res_list = pool.map(
-        lambda y0: integrator(func, y0, tseq, dt, Dfunc),
-        y0s)
-    pool.close()
-    pool.join()
-
-    return np.atleast_3d(res_list)
-
-
-def threads(func, y0s, tseq, dt, Dfunc=None,
-            integrator=inversion.integrators.scipy_odeint):
-    """Integrate each y0 in `y0s` to times in `tseq`.
-
-    Break up the work across :const:`MAX_PARALLEL` threads.  Not
-    sure if :func:`scipy.integrators.odeint` is threadsafe enough for
-    this application.  I know the object oriented solvers are not.
-
-    Parameters
-    ----------
-    func: callable(y, t)
-    y0s: np.ndarray[K, N]
-    tseq: sequence[M]
-        tseq[0] := t0
-    Dfunc: callable(y, t)
-        Dfunc[i, j] = d func[i]/d y[j]
-    integrator: callable(func, y0s, tseq, dt, Dfunc
-        The integrator to use
-
-    Returns
-    -------
-    sol_seqs: np.ndarray[M, K, N]
-        Array of solutions to y' = func(y, t)
-    """
-    y0s = np.atleast_2d(y0s)
-    res = np.empty((len(tseq),) + y0s.shape, y0s.dtype)
-
-    def thread_fun(i):
-        """The function for the threads.
-
-        Assumes anything not thread-local is global.
+    def __init__(self, integrator):
+        """Set up to integrate.
 
         Parameters
         ----------
-        i: the thread id
+        integrator: callable
+            The integrator to use.
+            Follows the signature of :func:`inversion.integrators.forward_euler`
+
         """
-        res[:, i, :] = integrator(func, y0s[i, :], tseq, dt, Dfunc)
+        self._integrator = integrator
 
-    pool = [threading.Thread(target=thread_fun, args=(i,))
-            for i in range(y0s.shape[0])]
+    @abc.abstractmethod
+    def __call__(self, func, y0s, tseq, dt, Dfunc=None):
+        """Integrate each y0 in `y0s` to times in `tseq`.
 
-    for thread in pool:
-        thread.start()
+        Parameters
+        ----------
+        func: callable(y, t)
+        y0s: np.ndarray[K, N]
+        tseq: sequence[M]
+            tseq[0] := t0
+        Dfunc: callable(y, t)
+            Dfunc[i, j] = d func[i]/d y[j]
 
-    for thread in pool:
-        thread.join()
+        Returns
+        -------
+        sol_seqs: np.ndarray[M, K, N]
+            Array of solutions to y' = func(y, t)
+        """
+        pass
 
-    return res
+class SerialEnsembleIntegrator(EnsembleIntegrator):
+    """Integrate the ensemble members one at a time."""
+
+    def __call__(self, func, y0s, tseq, dt, Dfunc=None):
+        """Integrate each y0 in `y0s` to times in `tseq`.
+
+        Parameters
+        ----------
+        func: callable(y, t)
+        y0s: np.ndarray[K, N]
+        tseq: sequence[M]
+            tseq[0] := t0
+        Dfunc: callable(y, t)
+            Dfunc[i, j] = d func[i]/d y[j]
+
+        Returns
+        -------
+        sol_seqs: np.ndarray[M, K, N]
+            Array of solutions to y' = func(y, t)
+        """
+        tseq = np.atleast_1d(tseq)
+        y0s = np.atleast_2d(y0s)
+
+        res = np.empty((len(tseq),) + y0s.shape, y0s.dtype)
+
+        integrator = self._integrator
+        for i in range(res.shape[1]):
+            res[:, i, :] = integrator(func, y0s[i, :], tseq, dt, Dfunc)
+
+        return res
+
+
+class MultiprocessEnsembleIntegrator(EnsembleIntegrator):
+    """Integrate the ensemble members in different processes.
+
+    Unrelaible on cygwin due to fork() failures.
+    """
+    
+    def __call__(self, func, y0s, tseq, dt, Dfunc=None):
+        """Integrate each y0 in `y0s` to times in `tseq`.
+
+        Break up the work across :const:`MAX_PARALLEL` processes.  Not
+        sure if :func:`scipy.integrators.odeint` is threadsafe enough for
+        this application.  I know the object oriented solvers are not.
+
+        Parameters
+        ----------
+        func: callable(y, t)
+        y0s: np.ndarray[K, N]
+        tseq: sequence[M]
+            tseq[0] := t0
+        Dfunc: callable(y, t)
+            Dfunc[i, j] = d func[i]/d y[j]
+        integrator: callable(func, y0s, tseq, dt, Dfunc
+            The integrator to use
+
+        Returns
+        -------
+        sol_seqs: np.ndarray[M, K, N]
+            Array of solutions to y' = func(y, t)
+
+        """
+        y0s = np.atleast_2d(y0s)
+
+        # Need the function to be picklable,
+        # so need to set these here.
+        self._func = func
+        self._tseq = tseq
+        self._dt = dt
+        self._Dfunc = Dfunc
+
+        pool = multiprocessing.Pool(MAX_PARALLEL)
+        res_list = pool.map(
+            self._integrate_one,
+            y0s)
+        pool.close()
+        pool.join()
+        # res_list is currently K, M, N
+
+        return np.swapaxes(res_list, 0, 1)
+
+    def _integrate_one(self, y0):
+        """Integrate a single member.
+
+        Parameters
+        ----------
+        y0: np.ndarray[N]
+
+        Returns
+        -------
+        np.ndarray[M, N]
+        """
+        return self._integrator(self._func, y0, self._tseq,
+                                self._dt, self._Dfunc)
+
+
+class ThreadedEnsembleIntegrator(EnsembleIntegrator):
+    """Integrate the ensemble members in different threads."""
+
+    def __call__(self, func, y0s, tseq, dt, Dfunc=None):
+        """Integrate each y0 in `y0s` to times in `tseq`.
+
+        Break up the work across :const:`MAX_PARALLEL` threads.  Not
+        sure if :func:`scipy.integrators.odeint` is threadsafe enough for
+        this application.  I know the object oriented solvers are not.
+
+        Parameters
+        ----------
+        func: callable(y, t)
+        y0s: np.ndarray[K, N]
+        tseq: sequence[T]
+            tseq[0] := t0
+        Dfunc: callable(y, t)
+            Dfunc[i, j] = d func[i]/d y[j]
+        integrator: callable(func, y0s, tseq, dt, Dfunc
+            The integrator to use
+
+        Returns
+        -------
+        sol_seqs: np.ndarray[T, K, N]
+            Array of solutions to y' = func(y, t)
+        """
+        y0s = np.atleast_2d(y0s)
+        res = np.empty((len(tseq),) + y0s.shape, y0s.dtype)
+
+        def thread_fun(i):
+            """The function for the threads.
+
+            Assumes anything not thread-local is global.
+
+            Parameters
+            ----------
+            i: the thread id
+            """
+            res[:, i, :] = integrator(func, y0s[i, :], tseq, dt, Dfunc)
+
+        pool = [threading.Thread(target=thread_fun, args=(i,))
+                for i in range(y0s.shape[0])]
+
+        for thread in pool:
+            thread.start()
+
+        for thread in pool:
+            thread.join()
+
+        return res
