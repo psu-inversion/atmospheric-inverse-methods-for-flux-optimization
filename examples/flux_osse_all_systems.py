b359bb8475c0cb85000ac5317a874025417f1115
Daniel Wesloh <dfw5129@psu.edu>
2018-01-20 10:21:09 -0500

    Try updating flux_osse_all_systems to work with current codebase.
    This combines the changes to use xarray for input with updates to the
    temporal correlations.

--- examples/flux_osse_all_systems.py
+++ examples/flux_osse_all_systems.py
@@ -12,22 +12,29 @@ For the fraternal twin setup, we use the different covariance matrices
 in the noise generation and the inversion.
 """
 
+import itertools
+import datetime
 import os.path
 import sys
 
 import numpy as np
 import numpy.linalg as la
+import dateutil.tz
 import scipy.linalg
 import matplotlib as mpl
 mpl.use("Agg")
 import matplotlib.pyplot as plt
+import pandas as pd
 
+import cf_units
+import cartopy.crs as ccrs
 import iris.cube
 import iris.util
 import iris.coords
 import iris.analysis.maths
 import iris.plot as iplt
 import iris.quickplot as qplt
+import xarray
 
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__),
@@ -60,8 +67,10 @@ TRUE_SP_ERROR_CORRELATION_FUN = (
     inversion.correlations.ExponentialCorrelation(TRUE_CORR_LEN))
 ASSUMED_SP_ERROR_CORRELATION_FUN = (
     inversion.correlations.ExponentialCorrelation(ASSUMED_CORR_LEN))
-TM_ERROR_CORRELATION_FUN = (
-    inversion.correlations.ExponentialCorrelation(2))
+DAY_ERROR_CORRELATION_FUN = (
+    inversion.correlations.ExponentialCorrelation(14))
+HOUR_ERROR_CORRELATION_FUN = (
+    inversion.correlations.ExponentialCorrelation(3))
 STDS = np.ones((N_FLUX_TIMES, NY, NX))
 
 COORD_ADJOINT_STR = "adjoint_"
@@ -75,80 +84,14 @@ FUNCTION_COORD = iris.coords.AuxCoord(
     long_name="inversion_function_name")
 
 DIVERGING_CMAP = plt.get_cmap("RdBu_r")
-
-def adjointize_coord(coord):
-    """Make an adjoint of coord.
-
-    Adds or removes "adjoint_" to the start of `coord.long_name`,
-    borrowing from :meth:`coord.name` if necessary.
-
-    Parameters
-    ----------
-    coord: iris.coords.Coord
-
-    Returns
-    iris.coords.Coord
-
-    """
-    result = coord.copy()
-    base_name = coord.long_name
-    if base_name is None:
-        base_name = coord.name()
-
-    if base_name.startswith(COORD_ADJOINT_STR):
-        result.long_name = base_name[len(COORD_ADJOINT_STR):]
-    else:
-        result.long_name = COORD_ADJOINT_STR + base_name
-
-    return result
+FLUX_FILE = "/mc1s2/s4/dfw5129/data/Marthas_2010_wrfouts/wrf_fluxes_all.nc"
 
 
-def adjointize_cube(cube):
-    """Adjoint of the cube.
-
-    :module:`iris` takes care of aligning axes, so it only really runs
-    :func:`adjointize_coord` on all the coordinates.
-
-    Parameters
-    ----------
-    cube: iris.cube.Cube
-
-    Returns
-    -------
-    iris.cube.Cube
-
-    """
-    result = cube.copy()
-
-    for coord in result.dim_coords:
-        new_coord = adjointize_coord(coord)
-        result.add_dim_coord(new_coord, cube.coord_dims(coord))
-        result.remove_coord(coord)
-
-    for coord in result.aux_coords:
-        new_coord = adjointize_coord(coord)
-        result.add_dim_coord(new_coord, cube.coord_dims(coord))
-        result.remove_coord(coord)
-
-    base_name = cube.long_name
-
-    if base_name is None:
-        base_name = cube.name()
-
-    if base_name.startswith(CUBE_ADJOINT_STR):
-        result.long_name = base_name[len(CUBE_ADJOINT_STR):]
-    else:
-        result.long_name = COORD_ADJOINT_STR + base_name
-
-    return result
-
-
-DX = iris.cube.Cube(1, units="km")
-X_COORD = iris.coords.DimCoord(np.arange(0, NX, 1), units="km",
+DX = iris.cube.Cube(27, units="km")
+X_COORD = iris.coords.DimCoord(DX.data * np.arange(0, NX), units="km",
                                standard_name="projection_x_coordinate")
-Y_COORD = iris.coords.DimCoord(np.arange(0, NY), units="km",
+Y_COORD = iris.coords.DimCoord(DX.data * np.arange(0, NY), units="km",
                                standard_name="projection_y_coordinate")
-DT = iris.cube.Cube(1, units="days")
 FLUX_TIME_COORD = iris.coords.DimCoord(
     np.arange(N_FLUX_TIMES),
     units="days since 2010-01-01 00:00:00+0000",
@@ -168,43 +111,87 @@ TOWER_COORD = iris.coords.DimCoord(
     long_name="tower_number",
 )
 
-#: thickness of the bottom layer in Pa
-THICKNESS = iris.cube.Cube(720., units="Pa")
-# source: wolfram alpha
-CO2_AIR_MASS_RATIO = iris.cube.Cube(
-    (.78084 * 2 * 14.0067 +
-     .20948 * 2 * 15.9994 +
-     # # this bit varys a lot.
-     # ~0% in deserts to ~5%? in tropics
-     # .01 * (2 * 1.00794 + 15.9994) +
-     .00934 * 39.948 +
-     .000380 * (12.0107 + 2*15.9994)) / (12.0107 + 2*15.9994),
-    units="(g/mol)/(g/mol)")
-# I think this is also from wolfram alpha
-EARTH_GRAVITY = iris.cube.Cube(9.807, units="m/s^2")
-GRAMS_TO_PPM = CO2_AIR_MASS_RATIO / (THICKNESS/EARTH_GRAVITY)
-"""Conversion from flux units to mixing ratio units
+FLUX_INTERVAL = 3
+"""The interval at which fluxes become available in hours.
 
-Assumes fluxes are in g/m^2/hr;
-I think this is independent of the actual area units,
-but I don't know
-Converts to flux tendency in ppmv/hr
+Fluxes are usually integrated forward from hourly input, but I can't
+solve for that in a reasonable timeframe.
 
-Notes
------
-.. math::
+This determines both the input and the output time resolution as well
+as what the inversion solves for.
 
-    F/M_{CO2}/dz = \\Delta n_{CO2} \\\\
-    dz = -dP/\\rho g \\\\
-    \\Delta X_{CO2} = \\Delta n_{CO2}/n_{air}
-                    = \\Delta n_{CO2}/ (\\rho_{air} / M_{air}) \\\\
-    \\Delta X_{CO2} = F/(M_{CO2} * -dP/(\\rho_{air} g) * M_{air}/\\rho_{air} \\\\
-    \\Delta X_{CO2} = F/M_{CO2} / (-dP/g) * M_{air}
+Note
+----
+Must divide twenty-four.
+"""
+FLUX_NAME = "E_TRA2"
+TRACER_NAME = "tracer_2_subset"
+FLUX_CHUNKS = 64
+"""How many flux times to treat at once.
 
-Need to convert F to kg if dP uses Pa
-X_{CO2} is here in units of 1; multiply by 1e6 to get ppmv
+Must be a multiple of day length.
+"""
+OBS_CHUNKS = 62
+"""How many observations to treat at once.
+
+Must allow a few chunks of the influence function to be in memory at
+once.  Will need to extend this if (N_OBS_TIMES * N_SITES) ** 2 array
+stops fitting in memory.
 """
 
+HOURS_PER_DAY = 24
+DAYS_PER_WEEK = 7
+FLUX_WINDOW = HOURS_PER_DAY * DAYS_PER_WEEK * 2
+"""How long fluxes considered to have an influence.
+
+Measured in hours.
+Implemented by slicing out this much of the stored influence functions
+for use as the linearized observation operator in the inversion.
+"""
+OBS_HOURS = (datetime.time(14), datetime.time(18))
+"""Which observation times will be used in the inversion.
+
+Assumed to be UTC. Should give afternoon hours for the domain.  Not
+currently set up for domains spanning many time zones. This shouldn't
+matter for OSSEs, but will for real-data cases (PBL schemes don't
+treat night well)
+
+I really hope I can assume this doesn't depend on latitude. That would
+make this much more complicated.
+"""
+OBS_TIMES_PER_DAY = OBS_HOURS[1].hour - OBS_HOURS[0].hour
+"""Observations used per site per day."""
+OBS_WINDOW = 1
+CO2_MOLAR_MASS = 16 * 2 + 12.01
+"""Molar mass of CO2 (g/mol).
+
+Used to convert WRF fluxes to units expected by observation operator.
+"""
+CO2_MOLAR_MASS_UNITS = cf_units.Unit("g/mol")
+FLUX_UNITS = cf_units.Unit("g/m^2/hr")
+
+
+def sort_key_to_consecutive(sequence):
+    """Turn a list of sort keys into a list of consecutive numbers.
+
+    Parameters
+    ----------
+    sequence: collections.Sequence
+        The list to sort.
+
+    Returns
+    -------
+    tuple
+        ``argsort(sequence)``, I think
+    """
+    items = list(enumerate(sorted(sequence)))
+
+    items.sort(key=lambda item: sequence.index(item[1]))
+    return tuple(item[0] for item in items)
+
+
+UTC = dateutil.tz.tzutc()
+SECONDS_PER_HOUR = 3600
 
 # I'm pretty sure this is the proper memory order for products to work
 # Unfortunately, this conflicts with the intuition I get from the
@@ -215,13 +202,22 @@ TRUE_SP_ERROR_CORRELATION = (
 ASSUMED_SP_ERROR_CORRELATION = (
     inversion.correlations.HomogeneousIsotropicCorrelation.from_function(
         ASSUMED_SP_ERROR_CORRELATION_FUN, (NY, NX)))
-TM_ERROR_CORRELATION = (
+HOUR_ERROR_CORRELATION = (
     inversion.correlations.HomogeneousIsotropicCorrelation.from_function(
-        TM_ERROR_CORRELATION_FUN, (N_FLUX_TIMES)))
+        HOUR_ERROR_CORRELATION_FUN, (8,)))
+DAY_ERROR_CORRELATION = (
+    inversion.correlations.make_matrix(
+        DAY_ERROR_CORRELATION_FUN, (N_FLUX_TIMES // 8,)))
 TRUE_ERROR_CORRELATION = inversion.util.kronecker_product(
-    TM_ERROR_CORRELATION, TRUE_SP_ERROR_CORRELATION)
+    DAY_ERROR_CORRELATION,
+    inversion.util.kronecker_product(
+        HOUR_ERROR_CORRELATION,
+        TRUE_SP_ERROR_CORRELATION))
 ASSUMED_ERROR_CORRELATION = inversion.util.kronecker_product(
-    TM_ERROR_CORRELATION, ASSUMED_SP_ERROR_CORRELATION)
+    DAY_ERROR_CORRELATION,
+    inversion.util.kronecker_product(
+        HOUR_ERROR_CORRELATION,
+        ASSUMED_SP_ERROR_CORRELATION))
 
 # Do this part before or after the kronecker product?
 STDS = np.ones(N_FLUX_TIMES * N_GRID_POINTS)
@@ -233,40 +229,159 @@ print(TRUE_ERROR_COVARIANCE.shape)
 OBS_STDS = np.ones((N_OBS_TIMES, N_SITES)) / 5
 OBSERVATION_COVARIANCE = np.diag(np.square(OBS_STDS.reshape(-1)))
 
-TRUE_ERR_COV_CUBE = iris.cube.Cube(
-    TRUE_ERROR_COVARIANCE.reshape(N_FLUX_TIMES, NY, NX,
-                                  N_FLUX_TIMES, NY, NX),
-    long_name="error_covariances",
-    units="(g/km^2/hr)^2",
-    dim_coords_and_dims=(
-        (FLUX_TIME_COORD, 0),
-        (Y_COORD, 1),
-        (X_COORD, 2),
-        (adjointize_coord(FLUX_TIME_COORD), 3),
-        (adjointize_coord(Y_COORD), 4),
-        (adjointize_coord(X_COORD), 5),
-    ),
-)
-ASSUMED_ERR_COV_CUBE = iris.cube.Cube(
-    ASSUMED_ERROR_COVARIANCE.reshape(N_FLUX_TIMES, NY, NX,
-                                     N_FLUX_TIMES, NY, NX),
-    long_name="error_covariances",
-    units="(g/km^2/hr)^2",
-    dim_coords_and_dims=(
-        (FLUX_TIME_COORD, 0),
-        (Y_COORD, 1),
-        (X_COORD, 2),
-        (adjointize_coord(FLUX_TIME_COORD), 3),
-        (adjointize_coord(Y_COORD), 4),
-        (adjointize_coord(X_COORD), 5),
-    ),
-)
-print(TRUE_ERR_COV_CUBE)
-
 # This one goes forward in time
-INFLUENCE_FUNCTION = iris.load_cube("/mc1s2/s4/dfw5129/data/LPEMLPDM_2010_01_03hrly_footprints_collapsed.nc4")
+INFLUENCE_DATASET = xarray.open_mfdataset(
+    "/mc1s2/s4/dfw5129/data/LPDM_2010_fpbounds/ACT-America_trial4/2010/01/GROUP1/LPDM_2010_01_03hrly_footprints.nc4",
+    # Total runtime by chunks
+    # 6m10s for 62, 4, 8*7*2, full
+    # 6m to segfault with 62, 4, 64, full
+    chunks=dict(observation_time=OBS_CHUNKS, site=N_SITES,
+                time_before_observation=FLUX_WINDOW // FLUX_INTERVAL,
+                dim_y=NY, dim_x=NX)).isel(
+    observation_time=slice(0, N_OBS_TIMES),
+    time_before_observation=slice(0, N_TIMES_BACK))
+INFLUENCE_FUNCTIONS = INFLUENCE_DATASET.H
+# Use site names as index/dim coord for site dim
+INFLUENCE_FUNCTIONS["site"] = np.char.decode(INFLUENCE_FUNCTIONS["site_names"].values, "ascii")
+
+OBS_TIME_INDEX = INFLUENCE_DATASET.indexes["observation_time"]
+TIME_BACK_INDEX = INFLUENCE_DATASET.indexes["time_before_observation"]
+
+# NB: Remember to change frequency and time zone as necessary.
+FLUX_START = (OBS_TIME_INDEX[-1] - TIME_BACK_INDEX[-1]).replace(
+    hour=0, microsecond=0, nanosecond=0)
+if OBS_TIME_INDEX[0].hour != 0:
+    FLUX_END = OBS_TIME_INDEX[0].replace(hour=0) + datetime.timedelta(days=1)
+else:
+    FLUX_END = OBS_TIME_INDEX[0]
+FLUX_TIMES_INDEX = pd.date_range(FLUX_START,
+                                 FLUX_END,
+                                 freq="{flux_interval:d}H".format(flux_interval=FLUX_INTERVAL),
+                                 tz="UTC",
+                                 closed="right",
+                                 name="flux_times")
+N_FLUX_TIMES = len(FLUX_TIMES_INDEX)
+
+FLUX_DATASET = xarray.open_mfdataset(
+    FLUX_FILE,
+    chunks=dict(projection_x_coordinate=NX, projection_y_coordinate=NY, XTIME=1),
+    concat_dim="Time",
+)
+# Many of the times are off by about four milliseconds.
+# This difference is irrelevant here.
+wrf_orig_times = FLUX_DATASET["XTIME"].to_index()
+timestamps = [timestamp.replace(microsecond=0,
+                                nanosecond=0)
+              for timestamp in wrf_orig_times]
+timestamps[-1] += datetime.timedelta(hours=FLUX_INTERVAL/2-1)
+timestamps[0] -= datetime.timedelta(hours=1)
+wrf_new_times = pd.DatetimeIndex(timestamps,
+                                 name="XTIME")
+FLUX_DATASET.coords["Time"] = wrf_new_times
+FLUX_DATASET.set_index(XTIME="Time", inplace=True)
+FLUX_DATASET = FLUX_DATASET.rename(
+    dict(
+        XTIME="Time", projection_y_coordinate="south_north",
+        projection_x_coordinate="west_east"),
+    inplace=True)
+# Assign a few more coords and pull out only the fluxes we need.
+FLUX_DATASET = FLUX_DATASET.sel(Time=FLUX_TIMES_INDEX)
+TRUE_FLUXES = FLUX_DATASET.get(["E_TRA{:d}".format(i+1)
+                                for i in range(10)]).isel(emissions_zdim=0)
+TRUE_FLUXES_MATCHED = TRUE_FLUXES.rename(dict(
+    south_north="dim_y", west_east="dim_x", Time="flux_time")) * CO2_MOLAR_MASS
+for flux_part, flux_orig in zip(TRUE_FLUXES_MATCHED.data_vars.values(), TRUE_FLUXES.data_vars.values()):
+    unit = (cf_units.Unit(flux_orig.attrs["units"]) *
+            CO2_MOLAR_MASS_UNITS)
+    # For whatever reason this is backwards from the conversion
+    # factors used elsewhere.
+    flux_part *= (unit / FLUX_UNITS).convert(1, 1)
+    flux_part.attrs["units"] = str(FLUX_UNITS)
+
+
+print(datetime.datetime.now(UTC).strftime("%c"), "Getting solar times")
+############################################################
+# Get time zone representing local solar time for each site
+LOCAL_TIME_ZONES = list(map(
+    dateutil.tz.tzoffset,
+    ("Local time for {name!s}".format(name=name)
+     for name in INFLUENCE_FUNCTIONS.coords["site"].values),
+    [round(offset_hr * SECONDS_PER_HOUR)
+     for offset_hr in INFLUENCE_FUNCTIONS.coords["site_lons"].values / 15]))
+
+print(datetime.datetime.now(UTC).strftime("%c"),
+      "Converting influence function to have alignment necessary for H")
+############################################################
+# Do the inversion
+############################################################
+# The Kalman filter version subtracts an hour from these. Why?
+obs_times = (INFLUENCE_FUNCTIONS.indexes["observation_time"][::-1])
+
+############################################################
+# Align observation flux times
+# Take care of missing obs
+# Also subsetting for late afternoon steady convective boundary layer
+# takes roughly 11 min.
+# Takes 6 min with everything in memory
+# Two minutes with big chunks?
+site_obs_index = []
+print(datetime.datetime.now(UTC).strftime("%c"), "Selecting observations")
+for i, site in enumerate(INFLUENCE_FUNCTIONS.indexes["site"]):
+    local_times = pd.Index([
+        obs_time.tz_localize(UTC)
+        .tz_convert(LOCAL_TIME_ZONES[i])
+        for obs_time in obs_times], name="local_times")
+
+    # np.where is designed for multi-dimensional arrays
+    site_obs_index.extend(zip(
+        np.where((OBS_HOURS[0] < local_times.time) &
+                 (local_times.time < OBS_HOURS[1]))[0],
+        itertools.repeat(site)))
+obs_index, site_index = zip(*site_obs_index)
+site_index = np.array(list(site_index))
+pd_obs_index = obs_times[list(obs_index)]
+site_obs_pd_index = pd.MultiIndex.from_tuples(
+    list(zip(site_index, pd_obs_index)), names=("site", "observation_time"))
+
+print(datetime.datetime.now(UTC).strftime("%c"),
+      "Aligning flux times in influence function")
+sys.stdout.flush()
+dimension_order = [item for item in INFLUENCE_FUNCTIONS.dims]
+dimension_order.insert(dimension_order.index("time_before_observation"), "flux_time")
+dimension_order.insert(dimension_order.index("observation_time"), "observation")
+dimension_order = tuple(dimension_order)
+aligned_influences = xarray.concat(
+    [here_infl.set_index(
+        time_before_observation="flux_time").rename(
+            dict(time_before_observation="flux_time"))
+     for here_infl in INFLUENCE_FUNCTIONS.sel_points(
+             site=site_index, observation_time=pd_obs_index)],
+    "observation").set_index(
+    observation=("observation_time", "site"))
+print(datetime.datetime.now(UTC).strftime("%c"), "Aligned flux times in influence function, aligning fluxes with influence function")
+sys.stdout.flush()
+aligned_influences, aligned_fluxes = xarray.align(aligned_influences, TRUE_FLUXES_MATCHED[FLUX_NAME],
+                                                  exclude=("dim_x", "dim_y"),
+                                                  join="outer", copy=False)
+# aligned_influences.reindex(flux_time=FLUX_TIMES_INDEX)
+print(datetime.datetime.now(UTC).strftime("%c"), "Aligned fluxes and influence function")
+aligned_fluxes = aligned_fluxes.chunk(dict(
+        dim_y=NY, dim_x=NX, flux_time=FLUX_CHUNKS))
+aligned_influences = aligned_influences.chunk(dict(
+        dim_y=NY, dim_x=NX, flux_time=FLUX_CHUNKS,
+        # One chunk per site.  Should be fast with the zeros in R.
+        observation=OBS_CHUNKS))
+print(datetime.datetime.now(UTC).strftime("%c"), "Rechunked to square")
+print("Aligned fluxes:", aligned_fluxes.dims, aligned_fluxes.shape)
+print(aligned_fluxes.chunks)
+print("Aligned influences:", aligned_influences.dims, aligned_influences.shape)
+print(aligned_influences.chunks)
+aligned_influences = aligned_influences.fillna(0)
+transpose_arg = sort_key_to_consecutive([dimension_order.index(dim)
+                                         for dim in aligned_influences.dims])
+
 OBSERVATION_OPERATOR = iris.cube.Cube(
-    INFLUENCE_FUNCTION[:N_OBS_TIMES, :N_SITES, :N_TIMES_BACK].lazy_data(),
+    INFLUENCE_FUNCTIONS[:N_OBS_TIMES, :N_SITES, :N_TIMES_BACK].data,
     long_name="influence_function",
     units="ppmv/(g/km^2/hr)",
     dim_coords_and_dims=(
@@ -295,34 +410,6 @@ OBSERVATION_OPERATOR = iris.cube.Cube(
             long_name=FLUX_TIME_COORD.long_name), (0, 2)),
     )
 )
-
-def coord_to_cube(coord):
-    """Turn the coord into a cube.
-
-    Parameters
-    ----------
-    coord: iris.coord.Coord
-
-    Returns
-    -------
-    iris.cube.Cube
-    """
-    result = iris.cube.Cube(
-        coord.points,
-        standard_name=coord.standard_name,
-        long_name=coord.long_name,
-        var_name=coord.var_name,
-        units=coord.units,
-        attributes=coord.attributes,
-        aux_coords_and_dims=(
-            (coord, range(len(coord.shape))),
-        ),
-    )
-    if len(coord.shape) == 1:
-        iris.util.promote_aux_coord_to_dim_coord(
-            result, coord.name())
-    return result
-
 
 tower_x = OBSERVATION_OPERATOR.coord(long_name="tower_x")
 tower_y = OBSERVATION_OPERATOR.coord(long_name="tower_y")
