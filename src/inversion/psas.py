33b642f4447d2cee9254b17d29c04438f9a9d962
Daniel Wesloh <dfw5129@psu.edu>
2018-08-05 06:52:19 -0400

    Remove dask .compute() from PSAS cost functions.
    I missed these in the main removal.

--- src/inversion/psas.py
+++ src/inversion/psas.py
@@ -96,7 +96,7 @@ def simple(background, background_covariance,
                 test_observation_increment)) -
             observation_increment.dot(
                 test_observation_increment)
-        ).compute()
+        )
 
     def cost_jacobian(test_observation_increment):
         """Gradient of cost function at `test_observation_increment`.
@@ -110,7 +110,7 @@ def simple(background, background_covariance,
         jac: np.ndarray[M]
         """
         return (covariance_sum.dot(test_observation_increment) -
-                observation_increment).compute()
+                observation_increment)
 
     if reduced_background_covariance is None:
         method = "BFGS"
@@ -251,7 +251,7 @@ def fold_common(background, background_covariance,
         """
         return (.5 * test_observation_increment.dot(covariance_sum.dot(
             test_observation_increment)) - observation_increment.dot(
-                test_observation_increment)).compute()
+                test_observation_increment))
 
     def cost_jacobian(test_observation_increment):
         """Gradient of cost function at `test_observation_increment`.
@@ -265,7 +265,7 @@ def fold_common(background, background_covariance,
         jac: np.ndarray[M]
         """
         return (covariance_sum.dot(test_observation_increment) -
-                observation_increment).compute()
+                observation_increment)
 
     if reduced_background_covariance is None:
         method = "BFGS"
