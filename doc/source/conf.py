714d1cf79968016b6d02673d1c12cb390ea9eb3d
Daniel Wesloh <22566757+DWesl@users.noreply.github.com>
2018-12-26 15:16:47 -0500

    Use the variable for the root page in the doc configuration.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Inversions'
-copyright = u'2017, Daniel Wesloh'
+copyright = u'2018, Daniel Wesloh'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -207,8 +207,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Inversions.tex', u'Inversions Documentation',
-   u'Daniel Wesloh', 'manual'),
+    (master_doc, 'Inversions.tex', u'Inversions Documentation',
+     u'Daniel Wesloh', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -237,7 +237,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'inversions', u'Inversions Documentation',
+    (master_doc, 'inversions', u'Inversions Documentation',
      [u'Daniel Wesloh'], 1)
 ]
 
@@ -251,9 +251,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Inversions', u'Inversions Documentation',
-   u'Daniel Wesloh', 'Inversions', 'Functions for geophysical inversions.',
-   'Miscellaneous'),
+    (master_doc, 'Inversions', u'Inversions Documentation',
+     u'Daniel Wesloh', 'Inversions', 'Functions for geophysical inversions.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
