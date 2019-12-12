14d1c4e1f5d98499a126dc6ba598538d85c23419
DWesl <22566757+DWesl@users.noreply.github.com>
2019-12-12 15:06:14 -0500

    Changes for second set of parameters/second run for paper.
    I really hope this works.

--- JAMES2019/run_inversion_osse.py
+++ JAMES2019/run_inversion_osse.py
@@ -60,7 +60,7 @@ Must divide twenty-four.
 """
 FLUX_RESOLUTION = 27
 """Resolution of fluxes and influence functions in kilometers."""
-UNCERTAINTY_RESOLUTION_REDUCTION_FACTOR = 3
+UNCERTAINTY_RESOLUTION_REDUCTION_FACTOR = 9
 """How much coarser uncertainty is than mean estimate in the x direction.
 
 If we compute the uncertainty at full resolution, the resulting file
@@ -73,7 +73,7 @@ report uncertainties within current computing constraints.
 UNCERTAINTY_FLUX_RESOLUTION = (UNCERTAINTY_RESOLUTION_REDUCTION_FACTOR *
                                FLUX_RESOLUTION * 1e3)
 """Resolution of posterior uncertainties in meters."""
-UNCERTAINTY_TEMPORAL_RESOLUTION = "1W"
+UNCERTAINTY_TEMPORAL_RESOLUTION = "1D"
 """The resolution at which the uncertainty is calculated and saved.
 
 Higher resolution means the uncertainties will be more accurate.
@@ -639,7 +639,9 @@ write_progress_message("Have combined correlations")
 # average seasonal variation (or some fraction thereof) might work.
 # x2 since MsTMIP spread does not represent full uncertainty
 # x5 since MsTMIP spread only represents monthly values and this uses sub-daily
-FLUX_VARIANCE_VARYING_FRACTION = 2. * 5.
+# x10 matches model-model for Raczka for 200km/21d
+# x3 matches model-model for 1000km/7d
+FLUX_VARIANCE_VARYING_FRACTION = 3.
 flux_std_pattern = xarray.open_dataset(
     "../data_files/2010_MsTMIP_flux_std.nc4",
     engine=NC_ENGINE
