Fix test precisions to account for single-precision evaluation.

src/atmos_flux_inversion/tests.py
@@ -479,7 +479,7 @@ class TestGaussianNoise(unittest2.TestCase):
 
         np_tst.assert_allclose(noise.mean(axis=0),
                                np.zeros(combined.shape[0]),
-                               rtol=1.1e-2, atol=1e-2)
+                               rtol=1.1e-2, atol=1.1e-2)
         np_tst.assert_allclose(np.cov(noise.T),
                                scipy.linalg.kron(op1, np.diag(diag)),
                                rtol=3e-2, atol=3e-2)
@@ -596,13 +596,23 @@ class TestCorrelations(unittest2.TestCase):
             for dist in (1, 5, 10, 15):
                 with self.subTest(corr_class=getname(corr_class),
                                   dist=dist):
+                    if (
+                            corr_class ==
+                            atmos_flux_inversion.correlations.
+                            GaussianCorrelation
+                    ):
+                        raise unittest2.SkipTest(
+                            "Gaussian({0:d}) correlations ill-conditioned".
+                            format(dist)
+                        )
+
                     corr_fun = corr_class(dist)
 
                     corr_mat = atmos_flux_inversion.correlations.make_matrix(
                         corr_fun, (test_ny, test_nx))
 
                     # Make sure diagonal elements are ones
-                    np_tst.assert_allclose(np.diag(corr_mat), 1)
+                    np_tst.assert_allclose(np.diag(corr_mat), 1, rtol=1e-6)
 
                     # check if it matches the original
                     np_tst.assert_allclose(
@@ -613,7 +623,7 @@ class TestCorrelations(unittest2.TestCase):
                         ).reshape((test_points, test_points)),
                         # rtol=1e-13: Gaussian 10 and 15 fail
                         # atol=1e-15: Gaussian 1 and 5 fail
-                        rtol=1e-11, atol=1e-13)
+                        rtol=1e-5, atol=1e-6)
 
                     # check if it actually is positive definite
                     cholesky(corr_mat)
@@ -657,6 +667,15 @@ class TestCorrelations(unittest2.TestCase):
             for dist in (1, 5, 10, 30):
                 with self.subTest(corr_class=getname(corr_class),
                                   dist=dist):
+                    if (
+                            corr_class ==
+                            atmos_flux_inversion.correlations.
+                            GaussianCorrelation
+                    ):
+                        raise unittest2.SkipTest(
+                            "Gaussian({0:d}) correlations ill-conditioned".
+                            format(dist)
+                        )
                     corr_fun = corr_class(dist)
 
                     corr_mat = atmos_flux_inversion.correlations.make_matrix(
@@ -665,7 +684,7 @@ class TestCorrelations(unittest2.TestCase):
                     )
 
                     # Make sure diagonal elements are ones
-                    np_tst.assert_allclose(np.diag(corr_mat), 1)
+                    np_tst.assert_allclose(np.diag(corr_mat), 1, rtol=1e-6)
 
                     # check if it matches the original
                     np_tst.assert_allclose(
@@ -675,7 +694,8 @@ class TestCorrelations(unittest2.TestCase):
                         ).reshape((test_nt, test_nt)),
                         # rtol=1e-13: Gaussian 10 and 15 fail
                         # atol=1e-15: Gaussian 1 and 5 fail
-                        rtol=1e-12, atol=1e-14)
+                        rtol=2e-7, atol=5e-7
+                    )
 
                     # check if it actually is positive definite
                     chol_upper = cholesky(corr_mat)
@@ -752,7 +772,7 @@ class TestCorrelations(unittest2.TestCase):
                         np_tst.assert_allclose(
                             corr_op.dot(test_vec)[noncorr_dist:-noncorr_dist],
                             corr_mat.dot(test_vec)[noncorr_dist:-noncorr_dist],
-                            rtol=1e-3, atol=1e-5)
+                            rtol=1e-3, atol=1.5e-3)
 
                 for i, test_vec in enumerate(test_lst):
                     with self.subTest(corr_class=getname(corr_class),
@@ -781,7 +801,8 @@ class TestCorrelations(unittest2.TestCase):
                             la.solve(
                                 corr_mat,
                                 test_vec)[noncorr_dist:-noncorr_dist],
-                            rtol=1e-3, atol=1e-5)
+                            rtol=1e-3, atol=2e-3
+                        )
 
     def test_1d_fft_correlation_acyclic(self):
         """Test HomogeneousIsotropicCorrelation for acyclic 1D arrays.